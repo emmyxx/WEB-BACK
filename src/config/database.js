87fb863007c969ac6a569944e64530ea69ad3403
emmyxx <emmy.alquie@etu.umontpellier.fr>
2023-05-29 18:27:47 +0200

    Modification variable environnement

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-
 module.exports = sequelize;
+
+const sequelize = new Sequelize(
+    process.env.PG_NAME,
+    process.env.PG_USERNAME,
+    process.env.PG_PASSWORD,
+    {
+        host: process.env.PG_HOST,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        port: process.env.PG_PORT,
+        logging: false
+    }
+
+);
